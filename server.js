4fe071ed427713d9171c449f6a756aca2af6eb2d
Matt Dalley <mattdalley@Matts-MacBook-Air.local>
2025-03-12 13:57:24 -0500

    feat: disable mobile scrolling and serve static page by default
    - Added viewport meta tag to prevent scaling and zooming
    - Added CSS properties to lock viewport and prevent scrolling
    - Updated server to serve static.html as default page

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 // Serve static files from the current directory
 app.use(express.static(__dirname));
 
-// Serve index.html for the root route
+// Serve static.html for the root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'static.html'));
 });
 
 // Log all requests to help with debugging
